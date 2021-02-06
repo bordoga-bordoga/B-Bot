cd27b4118c2356302b22785d15eea93515411ba6
bordoga_bordoga <78116334+bordoga-bordoga@users.noreply.github.com>
2021-02-06 03:56:08 +0100

    Delete dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("./config");
-const bot_client_1 = __importDefault(require("./client/bot-client"));
-const client = new bot_client_1.default({ token: config_1.token, owners: config_1.owners });
-client.start();
