ac84c6ebb6a4c323be0e4b84274d71ca23f3f4bd
bordoga_bordoga <kolinjahin@gmail.com>
2021-02-15 15:21:27 +0100

    .

--- src/commands/util/announcements.ts
+++ src/commands/util/announcements.ts
@@ -1,5 +1,6 @@
 import { Command } from "discord-akairo";
 import { Message, MessageEmbed, TextChannel } from "discord.js";
+import { prefix } from "../../config";
 
 
 export default class Announcements extends Command {
@@ -9,7 +10,7 @@ export default class Announcements extends Command {
       aliases: ["announce", "post", "p"],
       category: "util",
       description: {
-        content: "Sends out an announcement to a specific channel with a message you w8rite.",
+        content: "Sends out an announcement to a specific channel with a message you write.",
         usage: "announce [ announcement message ]",
         examples: [
           "?announce This is a test announcement.",
@@ -23,24 +24,41 @@ export default class Announcements extends Command {
 
   public async exec(message: Message): Promise<void | Message> {
 
-    const Announcement_Message = message.content.substring(3);
+    const Announcement_Message = message.content.slice(prefix.length).replace(/announce|post|p/i, '');
     const destination = this.client.channels.cache.get('782491581429055519');
 
-    (destination as TextChannel).send(Announcement_Message + '\n\n<@&711229532576677939>').then(ghostPing => {
-      ghostPing.delete()
-    });
-
-    /*this.client.on('messageReactionAdd', (reaction, user) => {
-      console.log('Reacted');
-    });*/
-
+    // The announcements embed.
     const announcement_Embed = new MessageEmbed()
       .setAuthor(message.author.username, message.author.avatarURL())
       .setTitle('\`[ server name ]\` announcements')
       .setDescription('⠀\n' + Announcement_Message + '\n⠀')
       .setFooter('This message notified [ server name ] members \nTo unsubscribe: ❎ \nTo subscribe: ✅');
-    return (destination as TextChannel).send(announcement_Embed).then(sentEmbed => {
+    (destination as TextChannel).send(announcement_Embed).then(sentEmbed => {
       sentEmbed.react('❎').then(() => sentEmbed.react('✅'));
+    });
+
+    // Reactions roles for the announcements.
+    //if (message.embed.title === '\`[ server name ]\` announcements') {
+    this.client.on('messageReactionAdd', async (reaction, user) => {
+      if (user.bot) return;
+      if (!reaction.message.guild) return;
+
+      if (reaction.emoji.name === '✅') {
+        await reaction.message.guild.members.cache.get(user.id).roles.add('809785186598256690');
+      } else if (reaction.emoji.name === '❎') {
+        await reaction.message.guild.members.cache.get(user.id).roles.remove('809785186598256690');
+      }
+    });
+    /*} else {
+      return;
+    }*/
+
+
+    // Ghost Ping message.
+    (destination as TextChannel).send(Announcement_Message + '\n\n<@&711229532576677939>').then(ghostPing => {
+      ghostPing.delete()
     });
   }
 }
+
+
--- to-do-list.ts
+++ to-do-list.ts
@@ -1,52 +1,54 @@
 /**
 * ----╲ COMMANDS ╲----
 
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 * ───\ UTIL \───
 
-*** 1. Multiple responses for ping command.
-*   2. Announcements command.
+* //1. Multiple responses for ping command.
+* 2. Announcements command.
 *   a. Reaction roles.
 *   b. No ping announcements.
 *   c. Delete most recent announcement.
 *   d. Edit most recent announcement.
+*   e. Don't send empty announcements.
+*   f. Add images to announcements.
 * 4. VC mover.
 *   a. Move an entire VC to anther VC.
 *   b. Move a member from a VC to another VC.
-*   c.
+*   c. ...
 * 5. Help command.
 * 6. Logger.
 
-﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
+﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
 * ───\ FUN \───
 
-** 1. User time command.
+* 1. User time command.
 * 2. List of users' time.
 * 3. Add user to time list.
 * 4. DM users command.
 * 5. Argument format converter.
 * 6. SEP search.
 
-﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
+﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
 * ───\ MODS \───
 
 * 1. Ban.
 * 2. Kick.
 * 3. Unban.
 
-﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
+﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
 * ───\ ADMIN \───
 
 * 1. Prefix change.
 
-﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
+﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
 * ───\ OWNER \───
 
 * 1. Offline.
 * 2. Reload.
 
-﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
+﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋﹋
 * ───\ TEST \───
 
 * 1.
